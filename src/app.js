a4f7a89dc6ea81e4831aac64d231d777e745a0c2
Matias Humano <Alan.humano@gmail.com>
2019-12-02 14:53:26 -0300

    Final

--- src/app.js
+++ src/app.js
@@ -1,33 +1,48 @@
+
 var express = require('express');
+
 var connection = require('./connection');
+
+var path = require('path');
 
 var app = express();
 
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-  res.render('home', { 
-    mensaje: '¡Hola ET 35!', 
-    texto: 'Esto también sale del servidor',
-    fecha: new Date()
-  });
-  res.render('home');
+  res.render('index');
 });
 
-app.get('/query', function(req, res) {
-  connection.query('SELECT ' + req.query.campos +  ' FROM estudiantes', function (error, results, fields) {
-    if (error) throw error;
-    res.json(results);
+function boton(){
+var tConsulta=text(document.getElementById("consulta").value)
+}
+
 app.post('/', function(req, res) {
   const query = req.body.query;
   connection.query(query, function (error, results, fields) {
-    res.render('home', {
-      resultados: JSON.stringify(results),
-      columnas: JSON.stringify(fields),
+    console.log(results);
+    console.log(fields);
+    res.render('index', {
+      results: results,
+      fields: fields,
       error: error
     });
   });
+});
+
+
+
+app.listen(3000, function() {
+  connection.connect();
+  console.log('Entrá a http://localhost:3000 desde tu navegador para ver qué devuelve esto');
+});
+
+process.on('SIGINT', function() {
+  console.log('Cerrando la conexión con la base de datos')
+  connection.end();
+  process.exit(1);
 });
